7e0ab812142c81245e01cc1b5f23c4460b22ef0f
nejihyuga1600 <nejihyuga1600@gmail.com>
2026-03-30 20:36:51 -0600

    disable SSR for globe page to fix prerender error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import LocationPage from "./plan/location/page";
+'use client';
+import dynamic from 'next/dynamic';
+
+const LocationPage = dynamic(() => import('./plan/location/page'), { ssr: false });
 
 export default function Home() {
   return <LocationPage />;
